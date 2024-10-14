2f08365f679113e5a092db876992eeacff6fad04
Tan Zhong Yan <tanzhongyan.passion@gmail.com>
2024-10-14 11:59:47 +0800

    Firebase implementation
    Setting up of firebase SDK and CLI.

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk51zep9W7jmO3zC6-lQJy6jeE6zMreis",
+  authDomain: "mappalette-9e0bd.firebaseapp.com",
+  projectId: "mappalette-9e0bd",
+  storageBucket: "mappalette-9e0bd.appspot.com",
+  messagingSenderId: "907670644284",
+  appId: "1:907670644284:web:e1dc720f9ae0644cd2c539"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
