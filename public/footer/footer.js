d20b18ba9d02504204733b24f6da9d9822b0da6f
WeiShenL <weishen.lo.2023@scis.smu.edu.sg>
2024-10-29 12:54:45 +0800

    create separate navbar and footer
    updated homepage, settings, profile pages to support the separate navbar and footer.

---
+++ public/footer/footer.js
@@ -0,0 +1,32 @@
+app.component('site-footer', {
+    template: `
+    <footer class="footer-wrapper text-center">
+        <div class="container p-5">
+            <!-- Grid row -->
+            <div class="row align-items-center">
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase fw-bold">MapPalette</h5>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                    <h6 class="fw-bold">Contact us</h6>
+                    <p class="mb-1">mappalette@gmail.com</p>
+                    <p class="mb-1">+65-687654321</p>
+                    <p>81 Victoria St, Singapore 188065</p>
+                </div>
+
+                <div class="col-lg-4 col-md-12 text-md-end">
+                    <a href="#" class="me-3"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="me-3"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#" class="me-3"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <hr class="my-4">
+            <div class="text-center">
+                <p class="mb-0">&copy; 2024. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+    `
+});
