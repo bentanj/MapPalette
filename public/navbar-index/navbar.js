06d7e57634e372e1e31d4422080807dd219f4e6e
bentanj <bentanj@gmail.com>
2024-11-05 14:55:11 +0800

    updated index, login, signup
    changed styling effects to ensure they're standardised

--- public/navbar-index/navbar.js
+++ public/navbar-index/navbar.js
@@ -34,15 +34,6 @@ app.component('nav-bar', {
                 <a class="navbar-brand" href="index.html">
                     <img src="resources/mappalettelogo.png" alt="MapPalette Logo" style="width: 40px; height: 40px">MapPalette
                 </a>
-                <button class="navbar-toggler" 
-                        type="button" 
-                        data-bs-toggle="collapse" 
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" 
-                        aria-expanded="false" 
-                        aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
